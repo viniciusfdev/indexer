97f09876a270056ca0654beea5285cc95356338c
Vinicius F <vinilima07@gmail.com>
2020-11-08 12:51:08 -0300

    atv12 atv13 atv14

--- tp2_indice/index/indexer.py
+++ tp2_indice/index/indexer.py
@@ -1,8 +1,10 @@
 from nltk.stem.snowball import SnowballStemmer
-from nltk.tokenize import word_tokenize
 from bs4 import BeautifulSoup
 import string
+import nltk
 import os
+
+nltk.download('punkt')
 
 class Cleaner:
     def __init__(self,stop_words_file:str,language:str,
@@ -32,7 +34,7 @@ class Cleaner:
         set_stop_words = set()
         with open(str_file, "r") as stop_words_file:
             for line in stop_words_file:
-                arr_words = line.split(",")
+                arr_words = line.split("\n")
                 [set_stop_words.add(word) for word in arr_words]
         return set_stop_words
     
@@ -42,18 +44,14 @@ class Cleaner:
     def word_stem(self,term:str):
         return self.stemmer.stem(term)
 
-
     def remove_accents(self,term: str) -> str:
         return term.translate(self.accents_translation_table)
 
-    def is_accent(self, term: str) -> str:
-        return self.remove_accents(term) == term
-
     def preprocess_word(self,term:str) -> str:
-        if self.perform_stop_words_removal and self.is_stop_word(term) and self.is_accent(term):
-            return None
-        
         term = str.lower(term)
+
+        if self.perform_stop_words_removal and self.is_stop_word(term):
+            return None
 
         if self.perform_accents_removal:
             term = self.remove_accents(term)  
@@ -69,17 +67,36 @@ class HTMLIndexer:
                         perform_stop_words_removal=True,
                         perform_accents_removal=True,
                         perform_stemming=True)
-    def __init__(self,index):
+    
+    def __init__(self, index: "HashIndex"):
         self.index = index
 
     def text_word_count(self,plain_text:str):
         dic_word_count = {}
-
+        
+        words = [self.cleaner.preprocess_word(w) for w in nltk.word_tokenize(plain_text)]
+        
+        for w in words:
+            if w in dic_word_count:
+                dic_word_count[w] = dic_word_count[w] + 1
+            elif w:
+                dic_word_count[w] = 1
         return dic_word_count
+
     def index_text(self,doc_id:int, text_html:str):
-        pass
+        plain_text = self.cleaner.html_to_plain_text(text_html) # (1)
+        dict_text = self.text_word_count(plain_text) #(2)
+        
+        for w, freq in dict_text.items():
+            self.index.index(w, doc_id, freq)
 
-    def index_text_dir(self,path:str):
-
+    def index_text_dir(self, path: str):
         for str_sub_dir in os.listdir(path):
             path_sub_dir = f"{path}/{str_sub_dir}"
+            for str_sub_sub in os.listdir(path_sub_dir):
+                file_path = f"{path_sub_dir}/{str_sub_sub}"
+                with open(file_path, "r") as idx_file:
+                    idx_file.read
+                    ID = int(str_sub_sub.split(".")[0])
+                    self.index_text(ID, idx_file)
+        
